b58a0d93d34169ff4ff7b1bf12df58957b987403
Jonas Arnfred <jonas@ifany.org>
2012-05-28 20:48:18 +0000

    Tried to implement a step thing

--- src/library/scala/util/parsing/combinator/debugging/DebugableParsers.scala
+++ src/library/scala/util/parsing/combinator/debugging/DebugableParsers.scala
@@ -18,26 +18,45 @@ trait DebugableParsers {
     val location: ParserLocation = NoParserLocation
   }
 
+  // Actions the user can take
+  abstract class Action 
+  case class Step extends Action        // Normal debugging step
+  case class StepIn extends Action      // Step into a function
+  case class StepOut extends Action     // Step out of a function
+  case class Quit extends Action        // Terminate the debugger
+
 
   object Controller {
 
-    def step[U](res: ParseResult[U]) : Unit = {
+    var lastAction : Action = Step
 
-      // Check if we should stop. For now we only stop at '|'s
-
-      
-      // Check if current location is child of last location
-    
+    def input : Action = {
+      // Print empty line and Ask for user input
+      println("\n>> ")
+      Console.readChar match {
+        case 'j'    => Step
+        case 'l'    => StepIn
+        case 'h'    => StepOut
+        case 'q'    => Quit
+      }
     }
 
-    def init() : Unit = {
+    def init : Unit = {
       // Print a few statements about how to control the debugger
       println("Welcome to the Combinator Parser Debugger")
       println("Controls: j: Step, l: Step in, h: Step out, q: quit")
 
       // There might be more things to initalize later one
     }
-  
+
+    // Decides what to do based on last action and current parser
+    def step(name : String, loc: debugging.ParserLocation) : Unit = lastStep match {
+      // Step isn't right, since it would stop at every '|' and not just the ones in the current scope
+      case Step     => if (name == "|") lastAction = input else () // If input is '|' in the same scope, then stop
+      case StepIn   => () // we need the parents to work for this
+      case StepOut  => () // also need the parent
+      case Quit     => exit(0) // Should be somewhere else
+    }
   
   }
   
@@ -77,6 +96,10 @@ trait DebugableParsers {
           Controller.init();
           init = false;
         }
+
+        // Leave control flow to the controller
+        Controller.step(name, location)
+
 
         println(in.pos.longString)
         // main access point for instrumenting the compiler
