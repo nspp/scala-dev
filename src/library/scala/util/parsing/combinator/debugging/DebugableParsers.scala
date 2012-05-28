3ed7c9a5e62e67a58e0969135aed9d1b2b93bac4
Jonas Arnfred <jonas@ifany.org>
2012-05-28 20:04:25 +0000

    Added an init msg

--- src/library/scala/util/parsing/combinator/debugging/DebugableParsers.scala
+++ src/library/scala/util/parsing/combinator/debugging/DebugableParsers.scala
@@ -11,10 +11,34 @@ trait DebugableParsers {
   self: Parsers =>
     
   val debug: Boolean = true//(sys.props.getOrElse("parser.combinators.debug", "false") == "true")
+  val init : Boolean = true;
   
   trait NoLocationParser[+T] {
     selfP: Parser[T] =>
     val location: ParserLocation = NoParserLocation
+  }
+
+
+  object Controller {
+
+    def step[U](res: ParseResult[U]) : Unit = {
+
+      // Check if we should stop. For now we only stop at '|'s
+
+      
+      // Check if current location is child of last location
+    
+    }
+
+    def init() : Unit = {
+      // Print a few statements about how to control the debugger
+      println("Welcome to the Combinator Parser Debugger")
+      println("Controls: j: Step, l: Step in, h: Step out, q: quit")
+
+      // There might be more things to initalize later one
+    }
+  
+  
   }
   
   trait DebugableParser[+T] {
@@ -22,11 +46,13 @@ trait DebugableParsers {
       
     val location: debugging.ParserLocation
     def ps: List[Parser[T]] = List() // TODO must respect the order
+    def ls: List[debugging.ParserLocation] = List()
       
     
     // TODO inline, or macro 
     def enter(): Unit = {
       if (debug && location != NoParserLocation) {
+
         // main access point for instrumenting the compiler
         // for now just print statements
         println("Enter parser")
@@ -45,15 +71,22 @@ trait DebugableParsers {
     
     def enterRes(in: Input): Unit = {
       if (debug && location != NoParserLocation) {
-        println("Input: " + in.toString)
+
+        // Simple check to see if we want to do an init message
+        if (init) {
+          Controller.init();
+          init = false;
+        }
+
+        println(in.pos.longString)
         // main access point for instrumenting the compiler
         // for now just print statements
 
-        println("Try to consume the token")
+        println("Try to consume token")
         println("[Name] " + name)
         println("[Location] " + location.line + ":" + location.offset)
         println("[File] " + location.fileName)
-        println("[Method] " + printMethod(location))
+        println("[Method] " + printMethod(location) + "\n")
       }
     }
 
